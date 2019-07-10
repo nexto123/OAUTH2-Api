5ff9015ee945188d9583083be456740c6cc7ee47
ernest <pristowel@gmail.com>
2019-07-10 18:30:16 +0100

    signup view created

--- mob_app/views.py
+++ mob_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.views.generic import (TemplateView,ListView,
+                                  DetailView,CreateView,
+                                  UpdateView,DeleteView)
+
+
+
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'mob_app/home.html'
