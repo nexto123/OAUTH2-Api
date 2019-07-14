d24a4ba0debda274df62fb40e825f65ea6a20bd2
ernest <pristowel@gmail.com>
2019-07-15 00:23:40 +0100

    further test with travis

--- api_made/settings/base.py
+++ api_made/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['oauth2-user.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','oauth2-user.herokuapp.com']
 
 
 # Application definition
