c9c8c881de9848f8724bd6c4c2b2453bd35569d8
ernest <pristowel@gmail.com>
2019-07-15 01:07:24 +0100

    sqlite setting updated

--- api_made/settings/base.py
+++ api_made/settings/base.py
@@ -97,13 +97,21 @@ if "DATABASE_URL" in os.environ:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-    print("Postgres URL not found, using sqlite instead")
+
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-}
+    }
+
+#     print("Postgres URL not found, using sqlite instead")
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+#         }
+# }
 
 
 # Password validation
